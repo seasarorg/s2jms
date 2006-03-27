1422922f8ae92577117ab0b01902bbd74c67ed35
y-komori <komo@littleforest.jp>
2006-03-27 15:19:32 +0000

    S2JMSTestCase とそれに伴うクラスを新規追加.
    git-svn-id: https://www.seasar.org/svn/s2jms/trunk@85 af07a43f-3802-0410-bdf4-81faf3a5335f

---
+++ s2jms/s2jms-container/src/main/java/org/seasar/jms/container/unit/S2JMSTestCase.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.jms.container.unit;
+
+import org.seasar.framework.container.ExternalContext;
+import org.seasar.framework.container.S2Container;
+import org.seasar.framework.container.deployer.ComponentDeployerFactory;
+import org.seasar.framework.unit.S2FrameworkTestCase;
+import org.seasar.jms.container.deployer.JMSComponentDeployerProvider;
+import org.seasar.jms.container.impl.JMSExternalContext;
+import org.seasar.jms.container.impl.JMSExternalContextComponentDefRegister;
+import org.seasar.jms.container.impl.JMSRequestImpl;
+
+/**
+ * @author y-komori
+ *
+ */
+public class S2JMSTestCase extends S2FrameworkTestCase {
+
+    @Override
+    protected void setUpContainer() throws Throwable {
+        super.setUpContainer();
+        
+        ExternalContext externalContext = new JMSExternalContext();
+        externalContext.setRequest(new JMSRequestImpl());
+        
+        S2Container container = getContainer();
+        container.setExternalContext(externalContext);
+        container.setExternalContextComponentDefRegister(
+                new JMSExternalContextComponentDefRegister());
+        
+        ComponentDeployerFactory.setProvider(new JMSComponentDeployerProvider());
+    }
+}
