497969d24ae0edbfa3398240eaee6b0e74527766
Koichi Kobayashi <koichik@improvement.jp>
2006-06-25 14:45:06 +0000

    JMS メッセージのヘッダ情報をリフレクションで扱うユーティリティクラスを追加．
    git-svn-id: https://www.seasar.org/svn/s2jms/trunk@179 af07a43f-3802-0410-bdf4-81faf3a5335f

---
+++ s2jms-core/src/main/java/org/seasar/jms/core/util/JMSHeaderSupport.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.jms.core.util;
+
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
+import javax.jms.Destination;
+import javax.jms.Message;
+
+import org.seasar.framework.util.ClassUtil;
+import org.seasar.framework.util.MethodUtil;
+import org.seasar.framework.util.tiger.CollectionsUtil;
+
+/**
+ * {@link java.jms.Message}のヘッダー情報にアクセスするためのユーティリティ。
+ * 
+ * @author koichik
+ */
+public class JMSHeaderSupport {
+
+    protected static final Set<String> headerNames;
+
+    protected static final Map<String, Method> getterMethods = CollectionsUtil
+            .newHashMap();
+
+    protected static final Map<String, Method> setterMethods = CollectionsUtil
+            .newHashMap();
+
+    static {
+        getterMethods.put("JMSCorrelationID", ClassUtil.getMethod(
+                Message.class, "getJMSCorrelationID", null));
+        getterMethods.put("JMSCorrelationIDAsBytes", ClassUtil.getMethod(
+                Message.class, "getJMSCorrelationIDAsBytes", null));
+        getterMethods.put("JMSDeliveryMode", ClassUtil.getMethod(Message.class,
+                "getJMSDeliveryMode", null));
+        getterMethods.put("JMSDestination", ClassUtil.getMethod(Message.class,
+                "getJMSDestination", null));
+        getterMethods.put("JMSExpiration", ClassUtil.getMethod(Message.class,
+                "getJMSExpiration", null));
+        getterMethods.put("JMSMessageID", ClassUtil.getMethod(Message.class,
+                "getJMSMessageID", null));
+        getterMethods.put("JMSPriority", ClassUtil.getMethod(Message.class,
+                "getJMSPriority", null));
+        getterMethods.put("JMSRedelivered", ClassUtil.getMethod(Message.class,
+                "getJMSRedelivered", null));
+        getterMethods.put("JMSReplyTo", ClassUtil.getMethod(Message.class,
+                "getJMSReplyTo", null));
+        getterMethods.put("JMSTimestamp", ClassUtil.getMethod(Message.class,
+                "getJMSTimestamp", null));
+        getterMethods.put("JMSType", ClassUtil.getMethod(Message.class,
+                "getJMSType", null));
+
+        setterMethods.put("JMSCorrelationID", ClassUtil.getMethod(
+                Message.class, "setJMSCorrelationID", new Class<?>[] {String.class}));
+        setterMethods.put("JMSCorrelationIDAsBytes", ClassUtil.getMethod(
+                Message.class, "setJMSCorrelationIDAsBytes", new Class<?>[] {byte[].class}));
+        setterMethods.put("JMSDeliveryMode", ClassUtil.getMethod(Message.class,
+                "setJMSDeliveryMode", new Class<?>[] {int.class}));
+        setterMethods.put("JMSDestination", ClassUtil.getMethod(Message.class,
+                "setJMSDestination", new Class<?>[] {Destination.class}));
+        setterMethods.put("JMSExpiration", ClassUtil.getMethod(Message.class,
+                "setJMSExpiration", new Class<?>[] {long.class}));
+        setterMethods.put("JMSMessageID", ClassUtil.getMethod(Message.class,
+                "setJMSMessageID", new Class<?>[] {String.class}));
+        setterMethods.put("JMSPriority", ClassUtil.getMethod(Message.class,
+                "setJMSPriority", new Class<?>[] {int.class}));
+        setterMethods.put("JMSRedelivered", ClassUtil.getMethod(Message.class,
+                "setJMSRedelivered", new Class<?>[] {boolean.class}));
+        setterMethods.put("JMSReplyTo", ClassUtil.getMethod(Message.class,
+                "setJMSReplyTo", new Class<?>[] {Destination.class}));
+        setterMethods.put("JMSTimestamp", ClassUtil.getMethod(Message.class,
+                "setJMSTimestamp", new Class<?>[] {long.class}));
+        setterMethods.put("JMSType", ClassUtil.getMethod(Message.class,
+                "setJMSType", new Class<?>[] {String.class}));
+
+        headerNames = Collections.unmodifiableSet(getterMethods.keySet());
+    }
+
+    /**
+     * {@link javax.jms.Message}ヘッダーの名前を要素とする変更不可能な{java.util.Set}を返します。
+     * 
+     * @return {@link javax.jms.Message}ヘッダーの名前を要素とする変更不可能な{java.util.Set}
+     */
+    public static Set<String> getNames() {
+        return headerNames;
+    }
+
+    /**
+     * <code>message</code>の<code>name</code>で指定されたヘッダーの値を返します。
+     * 
+     * @param message
+     *            メッセージ
+     * @param name
+     *            ヘッダー名
+     * @return <code>message</code>の<code>name</code>で指定されたヘッダーの値
+     */
+    public static Object getValue(final Message message, final String name) {
+        if (!getterMethods.containsKey(name)) {
+            return null;
+        }
+        final Method getter = getterMethods.get(name);
+        return MethodUtil.invoke(getter, message, null);
+    }
+
+    /**
+     * <code>message</code>の<code>name</code>で指定されたヘッダーの値を設定します。
+     * 
+     * @param message
+     *            メッセージ
+     * @param name
+     *            ヘッダー名
+     * @param value
+     *            ヘッダーに設定する値
+     */
+    public static void setValue(final Message message, final String name, final Object value) {
+        if (!setterMethods.containsKey(name)) {
+            return;
+        }
+        final Method setter = setterMethods.get(name);
+        MethodUtil.invoke(setter, message, new Object[] {value});
+    }
+
+}
---
+++ s2jms-core/src/test/java/org/seasar/jms/core/util/JMSHeaderSupportTest.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.jms.core.util;
+
+import java.util.Set;
+
+import javax.jms.Destination;
+import javax.jms.Message;
+
+import org.seasar.framework.unit.EasyMockTestCase;
+
+import static org.easymock.EasyMock.expect;
+
+;
+
+public class JMSHeaderSupportTest extends EasyMockTestCase {
+    static final byte[] BYTES = "hoge".getBytes();
+
+    Message message;
+
+    Destination destination;
+
+    Destination replyTo;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        message = createStrictMock(Message.class);
+        destination = createStrictMock(Destination.class);
+        replyTo = createStrictMock(Destination.class);
+    }
+
+    public void testHeaderNames() throws Exception {
+        Set<String> headerNames = JMSHeaderSupport.getNames();
+        assertEquals(11, headerNames.size());
+        assertTrue(headerNames.contains("JMSCorrelationID"));
+        assertTrue(headerNames.contains("JMSCorrelationIDAsBytes"));
+        assertTrue(headerNames.contains("JMSDeliveryMode"));
+        assertTrue(headerNames.contains("JMSDestination"));
+        assertTrue(headerNames.contains("JMSExpiration"));
+        assertTrue(headerNames.contains("JMSMessageID"));
+        assertTrue(headerNames.contains("JMSPriority"));
+        assertTrue(headerNames.contains("JMSRedelivered"));
+        assertTrue(headerNames.contains("JMSReplyTo"));
+        assertTrue(headerNames.contains("JMSTimestamp"));
+        assertTrue(headerNames.contains("JMSType"));
+        try {
+            headerNames.add("hoge");
+            fail();
+        } catch (UnsupportedOperationException expected) {
+        }
+    }
+
+    public void testGetValue() throws Exception {
+        new Subsequence() {
+            @Override
+            protected void replay() throws Exception {
+                assertEquals("hoge", JMSHeaderSupport.getValue(message,
+                        "JMSCorrelationID"));
+                assertEquals(BYTES, JMSHeaderSupport.getValue(message,
+                        "JMSCorrelationIDAsBytes"));
+                assertEquals(Message.DEFAULT_DELIVERY_MODE, JMSHeaderSupport
+                        .getValue(message, "JMSDeliveryMode"));
+                assertEquals(destination, JMSHeaderSupport.getValue(message,
+                        "JMSDestination"));
+                assertEquals(Message.DEFAULT_TIME_TO_LIVE, JMSHeaderSupport
+                        .getValue(message, "JMSExpiration"));
+                assertEquals("100", JMSHeaderSupport.getValue(message,
+                        "JMSMessageID"));
+                assertEquals(Message.DEFAULT_PRIORITY, JMSHeaderSupport
+                        .getValue(message, "JMSPriority"));
+                assertEquals(true, JMSHeaderSupport.getValue(message,
+                        "JMSRedelivered"));
+                assertEquals(replyTo, JMSHeaderSupport.getValue(message,
+                        "JMSReplyTo"));
+                assertEquals(10000L, JMSHeaderSupport.getValue(message,
+                        "JMSTimestamp"));
+                assertEquals("javax.jms.MapMessage", JMSHeaderSupport.getValue(
+                        message, "JMSType"));
+            }
+
+            @Override
+            protected void record() throws Exception {
+                expect(message.getJMSCorrelationID()).andReturn("hoge");
+                expect(message.getJMSCorrelationIDAsBytes()).andReturn(BYTES);
+                expect(message.getJMSDeliveryMode()).andReturn(
+                        Message.DEFAULT_DELIVERY_MODE);
+                expect(message.getJMSDestination()).andReturn(destination);
+                expect(message.getJMSExpiration()).andReturn(
+                        Message.DEFAULT_TIME_TO_LIVE);
+                expect(message.getJMSMessageID()).andReturn("100");
+                expect(message.getJMSPriority()).andReturn(
+                        Message.DEFAULT_PRIORITY);
+                expect(message.getJMSRedelivered()).andReturn(true);
+                expect(message.getJMSReplyTo()).andReturn(replyTo);
+                expect(message.getJMSTimestamp()).andReturn(10000L);
+                expect(message.getJMSType()).andReturn("javax.jms.MapMessage");
+            }
+        }.doTest();
+    }
+
+    public void testSetValue() throws Exception {
+        new Subsequence() {
+            @Override
+            protected void replay() throws Exception {
+                JMSHeaderSupport.setValue(message, "JMSCorrelationID", "hoge");
+                JMSHeaderSupport.setValue(message, "JMSCorrelationIDAsBytes",
+                        BYTES);
+                JMSHeaderSupport.setValue(message, "JMSDeliveryMode",
+                        Message.DEFAULT_DELIVERY_MODE);
+                JMSHeaderSupport.setValue(message, "JMSDestination",
+                        destination);
+                JMSHeaderSupport.setValue(message, "JMSExpiration",
+                        Message.DEFAULT_TIME_TO_LIVE);
+                JMSHeaderSupport.setValue(message, "JMSMessageID", "100");
+                JMSHeaderSupport.setValue(message, "JMSPriority",
+                        Message.DEFAULT_PRIORITY);
+                JMSHeaderSupport.setValue(message, "JMSRedelivered", true);
+                JMSHeaderSupport.setValue(message, "JMSReplyTo", replyTo);
+                JMSHeaderSupport.setValue(message, "JMSTimestamp", 10000L);
+                JMSHeaderSupport.setValue(message, "JMSType",
+                        "javax.jms.MapMessage");
+            }
+
+            @Override
+            protected void record() throws Exception {
+                message.setJMSCorrelationID("hoge");
+                message.setJMSCorrelationIDAsBytes(BYTES);
+                message.setJMSDeliveryMode(Message.DEFAULT_DELIVERY_MODE);
+                message.setJMSDestination(destination);
+                message.setJMSExpiration(Message.DEFAULT_TIME_TO_LIVE);
+                message.setJMSMessageID("100");
+                message.setJMSPriority(Message.DEFAULT_PRIORITY);
+                message.setJMSRedelivered(true);
+                message.setJMSReplyTo(replyTo);
+                message.setJMSTimestamp(10000L);
+                message.setJMSType("javax.jms.MapMessage");
+            }
+        }.doTest();
+    }
+}
