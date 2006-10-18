3e93f1d83529a6fae27595db6c173cdee1304668
Koichi Kobayashi <koichik@improvement.jp>
2006-10-18 17:46:45 +0000

    Message のモックを S2JMS-Container から S2JMS-Core へ移動．
    git-svn-id: https://www.seasar.org/svn/s2jms/trunk@204 af07a43f-3802-0410-bdf4-81faf3a5335f

---
+++ s2jms-core/src/main/java/org/seasar/jms/core/mock/BytesMessageMock.java
@@ -0,0 +1,370 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.jms.core.mock;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import javax.jms.BytesMessage;
+import javax.jms.JMSException;
+import javax.jms.MessageFormatException;
+import javax.jms.MessageNotReadableException;
+import javax.jms.MessageNotWriteableException;
+
+/**
+ * {@link javax.jms.BytesMessage}のモックです。
+ * <p>
+ * {@link javax.jms.BytesMessage}は二つの状態を持ちます。
+ * <dl>
+ * <dt>writable</dt>
+ * <dd>メッセージにデータを設定できる状態です</dd>
+ * <dt>readable</dt>
+ * <dd>メッセージからデータを読み出せる状態です</dd>
+ * </dl>
+ * モックオブジェクトをデフォルトコンストラクタで構築するとwritable状態のインスタンスが作成されます。 データを設定した後、
+ * {@link #reset()}メソッドを呼び出すことにより、 readable状態に遷移します。<br>
+ * モックオブジェクトを{@link #BytesMessageMock(byte[])}で構築するとreadable状態のインスタンスが作成されます。
+ * </p>
+ * 
+ * @author koichik
+ */
+public class BytesMessageMock extends MessageMock implements BytesMessage {
+
+    public enum State {
+        WRITABLE, READABLE
+    };
+
+    protected State state;
+    protected byte[] bytes;
+    protected ByteArrayOutputStream baos;
+    protected DataOutputStream os;
+    protected ByteArrayInputStream bais;
+    protected DataInputStream is;
+
+    /**
+     * writable状態のインスタンスを構築します。
+     */
+    public BytesMessageMock() {
+        state = State.WRITABLE;
+        baos = new ByteArrayOutputStream();
+        os = new DataOutputStream(baos);
+    }
+
+    /**
+     * バイト列を指定してreadable状態のインスタンスを構築します。
+     * 
+     * @param bytes
+     *            バイト列
+     */
+    public BytesMessageMock(final byte[] bytes) {
+        this.bytes = bytes;
+        state = State.READABLE;
+        bais = new ByteArrayInputStream(bytes);
+        is = new DataInputStream(bais);
+    }
+
+    /**
+     * インスタンスがwritable状態であることを検証します。
+     * 
+     * @throws MessageNotWriteableException
+     *             インスタンスがwritable状態でない場合にスローされます
+     */
+    protected void assertWritable() throws MessageNotWriteableException {
+        if (state != State.WRITABLE) {
+            throw new MessageNotWriteableException("");
+        }
+    }
+
+    /**
+     * インスタンスがreadable状態であることを検証します。
+     * 
+     * @throws MessageNotReadableException
+     *             インスタンスがreadable状態でない場合にスローされます
+     */
+    protected void assertReadable() throws MessageNotReadableException {
+        if (state != State.READABLE) {
+            throw new MessageNotReadableException("");
+        }
+    }
+
+    protected JMSException newJMSException(final Throwable cause) throws JMSException {
+        final JMSException e = new JMSException("exception occurd");
+        e.initCause(cause);
+        throw e;
+    }
+
+    public long getBodyLength() throws JMSException {
+        assertReadable();
+        return bytes.length;
+    }
+
+    public boolean readBoolean() throws JMSException {
+        assertReadable();
+        try {
+            return is.readBoolean();
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public byte readByte() throws JMSException {
+        assertReadable();
+        try {
+            return is.readByte();
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public int readUnsignedByte() throws JMSException {
+        assertReadable();
+        try {
+            return is.readByte();
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public short readShort() throws JMSException {
+        assertReadable();
+        try {
+            return is.readShort();
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public int readUnsignedShort() throws JMSException {
+        assertReadable();
+        try {
+            return is.readShort();
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public char readChar() throws JMSException {
+        assertReadable();
+        try {
+            return is.readChar();
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public int readInt() throws JMSException {
+        assertReadable();
+        try {
+            return is.readInt();
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public long readLong() throws JMSException {
+        assertReadable();
+        try {
+            return is.readLong();
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public float readFloat() throws JMSException {
+        assertReadable();
+        try {
+            return is.readFloat();
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public double readDouble() throws JMSException {
+        assertReadable();
+        try {
+            return is.readDouble();
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public String readUTF() throws JMSException {
+        assertReadable();
+        try {
+            return is.readUTF();
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public int readBytes(final byte[] bytes) throws JMSException {
+        assertReadable();
+        try {
+            return is.read(bytes);
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public int readBytes(final byte[] bytes, final int length) throws JMSException {
+        assertReadable();
+        try {
+            return is.read(bytes, 0, length);
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public void writeBoolean(final boolean value) throws JMSException {
+        assertWritable();
+        try {
+            os.writeBoolean(value);
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public void writeByte(final byte value) throws JMSException {
+        assertWritable();
+        try {
+            os.writeByte(value);
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public void writeShort(final short value) throws JMSException {
+        assertWritable();
+        try {
+            os.writeShort(value);
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public void writeChar(final char value) throws JMSException {
+        assertWritable();
+        try {
+            os.writeChar(value);
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public void writeInt(final int value) throws JMSException {
+        assertWritable();
+        try {
+            os.writeInt(value);
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public void writeLong(final long value) throws JMSException {
+        assertWritable();
+        try {
+            os.writeLong(value);
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public void writeFloat(final float value) throws JMSException {
+        assertWritable();
+        try {
+            os.writeFloat(value);
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public void writeDouble(final double value) throws JMSException {
+        assertWritable();
+        try {
+            os.writeDouble(value);
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public void writeUTF(final String value) throws JMSException {
+        assertWritable();
+        try {
+            os.writeUTF(value);
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public void writeBytes(final byte[] value) throws JMSException {
+        assertWritable();
+        try {
+            os.write(value);
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public void writeBytes(final byte[] value, final int offset, final int length)
+            throws JMSException {
+        assertWritable();
+        try {
+            os.write(value, offset, length);
+        } catch (final IOException e) {
+            throw newJMSException(e);
+        }
+    }
+
+    public void writeObject(final Object value) throws JMSException {
+        assertWritable();
+        if (Byte.class.isInstance(value)) {
+            writeByte(Byte.class.cast(value));
+        } else if (Short.class.isInstance(value)) {
+            writeShort(Short.class.cast(value));
+        } else if (Integer.class.isInstance(value)) {
+            writeInt(Integer.class.cast(value));
+        } else if (Long.class.isInstance(value)) {
+            writeLong(Long.class.cast(value));
+        } else if (Float.class.isInstance(value)) {
+            writeFloat(Float.class.cast(value));
+        } else if (Double.class.isInstance(value)) {
+            writeDouble(Double.class.cast(value));
+        } else if (Boolean.class.isInstance(value)) {
+            writeBoolean(Boolean.class.cast(value));
+        } else if (Character.class.isInstance(value)) {
+            writeChar(Character.class.cast(value));
+        } else if (String.class.isInstance(value)) {
+            writeUTF(String.class.cast(value));
+        } else if (byte[].class.isInstance(value)) {
+            writeBytes(byte[].class.cast(value));
+        } else {
+            throw new MessageFormatException("");
+        }
+    }
+
+    public void reset() throws JMSException {
+        assertWritable();
+        state = State.READABLE;
+        bytes = baos.toByteArray();
+        is = new DataInputStream(bais);
+        baos = null;
+        os = null;
+    }
+
+}
---
+++ s2jms-core/src/main/java/org/seasar/jms/core/mock/MessageMock.java
@@ -0,0 +1,244 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.jms.core.mock;
+
+import java.util.Enumeration;
+import java.util.Map;
+
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+
+import org.seasar.framework.util.EnumerationAdapter;
+import org.seasar.framework.util.tiger.CollectionsUtil;
+
+/**
+ * {@link javax.jms.Message} のモックです。
+ * 
+ * @author koichik
+ */
+public class MessageMock implements Message {
+
+    protected String JMSCorrelationID;
+
+    protected byte[] JMSCorrelationIDAsBytes;
+
+    protected int JMSDeliveryMode = DEFAULT_DELIVERY_MODE;
+
+    protected Destination JMSDestination;
+
+    protected long JMSExpiration = DEFAULT_TIME_TO_LIVE;
+
+    protected String JMSMessageID;
+
+    protected int JMSPriority = DEFAULT_PRIORITY;
+
+    protected boolean JMSRedelivered;
+
+    protected Destination JMSReplyTo;
+
+    protected long JMSTimestamp;
+
+    protected String JMSType;
+
+    protected Map<String, Object> properties = CollectionsUtil.newHashMap();
+
+    /**
+     * インスタンスを構築します。
+     */
+    public MessageMock() {
+    }
+
+    public void acknowledge() throws JMSException {
+    }
+
+    public void clearBody() throws JMSException {
+    }
+
+    public void clearProperties() throws JMSException {
+        properties.clear();
+    }
+
+    public boolean getBooleanProperty(final String name) throws JMSException {
+        return Boolean.class.cast(properties.get(name));
+    }
+
+    public byte getByteProperty(final String name) throws JMSException {
+        return Byte.class.cast(properties.get(name));
+    }
+
+    public double getDoubleProperty(final String name) throws JMSException {
+        return Double.class.cast(properties.get(name));
+    }
+
+    public float getFloatProperty(final String name) throws JMSException {
+        return Float.class.cast(properties.get(name));
+    }
+
+    public int getIntProperty(final String name) throws JMSException {
+        return Integer.class.cast(properties.get(name));
+    }
+
+    public String getJMSCorrelationID() throws JMSException {
+        return JMSCorrelationID;
+    }
+
+    public byte[] getJMSCorrelationIDAsBytes() throws JMSException {
+        return JMSCorrelationIDAsBytes;
+    }
+
+    public int getJMSDeliveryMode() throws JMSException {
+        return JMSDeliveryMode;
+    }
+
+    public Destination getJMSDestination() throws JMSException {
+        return JMSDestination;
+    }
+
+    public long getJMSExpiration() throws JMSException {
+        return JMSExpiration;
+    }
+
+    public String getJMSMessageID() throws JMSException {
+        return JMSMessageID;
+    }
+
+    public int getJMSPriority() throws JMSException {
+        return JMSPriority;
+    }
+
+    public boolean getJMSRedelivered() throws JMSException {
+        return JMSRedelivered;
+    }
+
+    public Destination getJMSReplyTo() throws JMSException {
+        return JMSReplyTo;
+    }
+
+    public long getJMSTimestamp() throws JMSException {
+        return JMSTimestamp;
+    }
+
+    public String getJMSType() throws JMSException {
+        return JMSType;
+    }
+
+    public long getLongProperty(final String name) throws JMSException {
+        return Long.class.cast(properties.get(name));
+    }
+
+    public Object getObjectProperty(final String name) throws JMSException {
+        return properties.get(name);
+    }
+
+    public Enumeration getPropertyNames() throws JMSException {
+        return new EnumerationAdapter(properties.keySet().iterator());
+    }
+
+    public short getShortProperty(final String name) throws JMSException {
+        return Short.class.cast(properties.get(name));
+    }
+
+    public String getStringProperty(final String name) throws JMSException {
+        return String.class.cast(properties.get(name));
+    }
+
+    public boolean propertyExists(final String name) throws JMSException {
+        return properties.containsKey(name);
+    }
+
+    public void setBooleanProperty(final String name, final boolean value) throws JMSException {
+        properties.put(name, value);
+    }
+
+    public void setByteProperty(final String name, final byte value) throws JMSException {
+        properties.put(name, value);
+    }
+
+    public void setDoubleProperty(final String name, final double value) throws JMSException {
+        properties.put(name, value);
+    }
+
+    public void setFloatProperty(final String name, final float value) throws JMSException {
+        properties.put(name, value);
+    }
+
+    public void setIntProperty(final String name, final int value) throws JMSException {
+        properties.put(name, value);
+    }
+
+    public void setJMSCorrelationID(final String JMSCorrelationID) throws JMSException {
+        this.JMSCorrelationID = JMSCorrelationID;
+    }
+
+    public void setJMSCorrelationIDAsBytes(final byte[] JMSCorrelationIDAsBytes)
+            throws JMSException {
+        this.JMSCorrelationIDAsBytes = JMSCorrelationIDAsBytes;
+    }
+
+    public void setJMSDeliveryMode(final int JMSDeliveryMode) throws JMSException {
+        this.JMSDeliveryMode = JMSDeliveryMode;
+    }
+
+    public void setJMSDestination(final Destination JMSDestination) throws JMSException {
+        this.JMSDestination = JMSDestination;
+    }
+
+    public void setJMSExpiration(final long JMSExpiration) throws JMSException {
+        this.JMSExpiration = JMSExpiration;
+    }
+
+    public void setJMSMessageID(final String JMSMessageID) throws JMSException {
+        this.JMSMessageID = JMSMessageID;
+    }
+
+    public void setJMSPriority(final int JMSPriority) throws JMSException {
+        this.JMSPriority = JMSPriority;
+    }
+
+    public void setJMSRedelivered(final boolean JMSRedelivered) throws JMSException {
+        this.JMSRedelivered = JMSRedelivered;
+    }
+
+    public void setJMSReplyTo(final Destination JMSReplyTo) throws JMSException {
+        this.JMSReplyTo = JMSReplyTo;
+    }
+
+    public void setJMSTimestamp(final long JMSTimestamp) throws JMSException {
+        this.JMSTimestamp = JMSTimestamp;
+    }
+
+    public void setJMSType(final String JMSType) throws JMSException {
+        this.JMSType = JMSType;
+    }
+
+    public void setLongProperty(final String name, final long value) throws JMSException {
+        properties.put(name, value);
+    }
+
+    public void setObjectProperty(final String name, final Object value) throws JMSException {
+        properties.put(name, value);
+    }
+
+    public void setShortProperty(final String name, final short value) throws JMSException {
+        properties.put(name, value);
+    }
+
+    public void setStringProperty(final String name, final String value) throws JMSException {
+        properties.put(name, value);
+    }
+
+}
