a040c12aba1ca133a6bfff6d3861d772f81f663d
Koichi Kobayashi <koichik@improvement.jp>
2006-12-04 14:01:42 +0000

    Sun Generic Reource Adapter 使用時に，現在のスレッドでメッセージを受信した Session を取得して返す SessionFactory の実装を追加しました．
    git-svn-id: https://www.seasar.org/svn/s2jms/trunk@211 af07a43f-3802-0410-bdf4-81faf3a5335f

---
+++ s2jms-container/src/main/java/org/seasar/jms/container/session/impl/GenericRaSessionFactoryImpl.java
@@ -0,0 +1,59 @@
+package org.seasar.jms.container.session.impl;
+
+import java.lang.reflect.Field;
+
+import javax.jms.JMSException;
+import javax.jms.Session;
+
+import org.seasar.framework.beans.BeanDesc;
+import org.seasar.framework.beans.factory.BeanDescFactory;
+import org.seasar.framework.util.FieldUtil;
+import org.seasar.jca.work.WorkWrapper;
+import org.seasar.jms.core.exception.SJMSRuntimeException;
+import org.seasar.jms.core.session.SessionFactory;
+import org.seasar.jms.core.session.SessionHandler;
+
+/**
+ * Java.netで公開されている<a href="https://genericjmsra.dev.java.net/">Generic
+ * Resource Adapter for JMS</a>を利用している場合に，現在処理中のメッセージを受信した{@link javax.jms.Session}を提供する
+ * {@link org.seasar.jms.core.session.SessionFactory}の実装クラスです．
+ * <p>
+ * このクラスは，Generic Resource Adapter for JMSとIBM WebSphere MQを組み合わせた場合に
+ * メッセージリスナーからWebSphere MQへメッセージを送信する場合に使うことを意図しています．
+ * </p>
+ * <p>
+ * IBM WebSphere MQは，メッセージを受信した{@link javax.jms.Session}に対するXAトランザクションが
+ * 開始されている状態で，メッセージを送信するために別の{@link javax.jms.Session}に対するXAトランザクションを
+ * {@link javax.transaction.xa.XAResource#TMJOIN}で開始しようとするとWebSphere
+ * MQのJMS実装ライブラリ内でロック待ちとなって固まってしまいます．そのため，メッセージリスナーからメッセージを
+ * 送信するには現在処理中のメッセージを受信した{@link javax.jms.Session}を利用する必要があります．
+ * </p>
+ * <p>
+ * そのために本クラスでは，現在処理中の{@link javax.resource.spi.work.Work}（実体は Generic RA
+ * の実装クラス)からリフレクションを使って{@link javax.jms.Session}を取得します．
+ * </p>
+ * <p>
+ * この実装はGeneric RA 1.5を対象にしています．
+ * </p>
+ * 
+ * @author koichik
+ * 
+ */
+public class GenericRaSessionFactoryImpl implements SessionFactory {
+
+    public void operateSession(final boolean startConnection, final SessionHandler handler) {
+        final Object work = WorkWrapper.getCurrentWork();
+        final BeanDesc workDesc = BeanDescFactory.getBeanDesc(work.getClass());
+        final Field jmsResourceField = workDesc.getField("jmsResource");
+        final Object jmsResource = FieldUtil.get(jmsResourceField, work);
+        final BeanDesc jmsResourceDesc = BeanDescFactory.getBeanDesc(jmsResource.getClass());
+        final Field sessionField = jmsResourceDesc.getField("session");
+        final Session session = Session.class.cast(FieldUtil.get(sessionField, jmsResource));
+        try {
+            handler.handleSession(session);
+        } catch (final JMSException e) {
+            throw new SJMSRuntimeException("EJMS0000", e);
+        }
+    }
+
+}
